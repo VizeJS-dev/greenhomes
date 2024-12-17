417937378bc103e68504eb715945bb5c51f7a8d7
Garik <garik.sandrosyan@gmail.com>
2024-12-17 15:54:01 +0100

    """ Set up initial Vite + React project with TailwindCSS
    Initialized a Vite-based React project with TailwindCSS, essential styling, and basic components. Added necessary dependencies and configurations for smooth development with HMR and ESLint. Included a simple navigation component and assets for initial UI structure.
    """

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'greenhomes-gray':'#D7D9D8',
+        'greenhomes-green':'#597351',
+        'greenhomes-darkgreen':'#45593F',
+        'greenhomes-orange':'#45593F',
+        'greenhomes-lightgreen':'#99A68D',
+      }
+    },
+  },
+  plugins: [],
+}
